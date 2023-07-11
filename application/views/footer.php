d8bbb31427471b6fa178509e27dc0a82e33344a6
Rsm-mah <mahrasamisoa@gmail.com>
2023-07-11 13:16:11 +0200

    modification à cause d'un probleme

--- application/views/footer.php
+++ application/views/footer.php
@@ -19,7 +19,9 @@
                         </p>
                     </div>
                     <div class="footer__copyright">
-                        <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p>
+                        <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                     </div>
                 </div>
             </div>
